804bfc5e4c9f64143f005b1df2b9e0b3d0030b3e
Nina Konopleva <orangemise@gmail.com>
2024-10-21 19:21:22 +0300

    Refactored App

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,7 @@
-import './App.css';
-import { Counter, Cards } from 'features';
-import { API_URL } from 'config';
-import { useState, useEffect } from 'react';
+import style from './App.module.scss';
+import { useEffect } from 'react';
+import { usePhotos, useTodos } from 'shared/hooks';
+import { Gallery, Tasks } from 'widgets';
 
 /**
  * @typedef {import('./types').AppProps} AppProps
@@ -14,51 +14,20 @@ import { useState, useEffect } from 'react';
  */
 
 export const App = (props) => {
-  const [count, setCount] = useState(1);
-  const [cards, setCard] = useState([]);
-  const [error, setError] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
+  const defaultCount = 5;
+  const photosState = usePhotos();
+  const todosState = useTodos();
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`${API_URL}photos?_start=0&_limit=${count}`);
-        const cardsFromAPI = await response.json();
-        setCard(cardsFromAPI);
-      } catch (/**@type {*}*/ error) {
-        setError(error.message);
-      };
-      setIsLoading(false);
-    };
-    fetchData();
-  }, [count]);
-
-  // useEffect(() => {
-  //   fetch(`${API_URL}photos?_start=0&_limit=${count}`)
-  //     .then((response) => response.json())
-  //     .then((cardsFromAPI) => setCard(cardsFromAPI))
-  //     .catch((error) => setError(error.message))
-  //     .finally(() => setIsLoading(false));
-  // }, [count]);
-
-  if (error) {
-    return <h1>Error: {error}</h1>;
-  };
+    photosState.setPhotoCount(defaultCount);
+    todosState.setTodoCount(defaultCount);
+  }, []);
 
   return (
-    <div className={'app'}>
+    <div className={style.app}>
       <h1>{props.name}</h1>
-      <Counter
-        count={count}
-        setCount={setCount}
-        minCount = {0}
-        maxCount = {100}
-      />
-      {isLoading ? (
-        <h1>Loading ......</h1>
-      ) : (
-        <Cards cards={cards} />
-      )}
+      <Gallery />
+      <Tasks />
     </div>
   );
 };
