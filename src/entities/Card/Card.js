0bbc6570324188ef35c236fafb31907c3e013908
Nina Konopleva <orangemise@gmail.com>
2024-11-03 23:17:26 +0300

    Refactored Card

--- src/entities/Card/Card.js
+++ src/entities/Card/Card.js
@@ -1,41 +1,99 @@
+import { Link } from 'react-router-dom';
 import style from './Card.module.scss';
 import { getRandomColor } from 'shared/utils/getRandomColor';
 
 /**
- * @typedef {import('./types').CardDetails} CardDetails
+ * @typedef {import('./types').PhotoDetails} PhotoDetails
+ * @typedef {import('./types').TodoDetails} TodoDetails
+ * @typedef {import('./types').PostDetail} PostDetail
+ * @typedef {import('./types').Card} Card
  */
 
 /**
+ * @function Photo
+ * @param {PhotoDetails} props
+ */
+
+const Photo = (props) => {
+  const { id, name, image } = props;
+  const endPoint = `/photo/${id}`;
+
+  return (
+    <>
+      <Link to={endPoint}>
+        <li className={style.card}
+          style={{ background: getRandomColor() }}
+        >
+          <h2 className={style.name}>
+            {name}
+          </h2>
+          <img className={style.image}
+            src={image}>
+          </img>
+        </li>
+      </Link>
+    </>
+  );
+};
+
+/**
+ * @function Todo
+ * @param {TodoDetails} props
+ */
+
+const Todo = (props) => {
+  const { id, name } = props;
+  const endPoint = `/todo/${id}`;
+  return (
+    <>
+      <Link to={endPoint}>
+        <li className={style.card}
+          style={{ background: getRandomColor() }}
+        >
+          <h2 className={style.name}>
+            {name}
+          </h2>
+        </li>
+      </Link>
+    </>
+  );
+};
+
+/**
+ * @function Post
+ * @param {PostDetail} props
+ */
+
+const Post = (props) => {
+  const { id, name, text } = props;
+  const endPoint = `/post/${id}`;
+  return (
+    <>
+      <Link to={endPoint}>
+        <li className={style.card}
+          style={{ background: getRandomColor() }}
+        >
+          <h2 className={style.namePost}>
+            {name}
+          </h2>
+          <p className={style.text}>
+            {text}
+          </p>
+        </li>
+      </Link>
+    </>
+  );
+};
+
+/**
  * @function Card
- * @param {CardDetails} props
+ * @param {Card} props
  * @returns {JSX.Element}
  */
 
-export const Card = (props) => {
-  const { id, name, image, text } = props;
-
-  return (
-    <li className={style.card}
-      style={{ background: getRandomColor() }}
-    >
-      {/* name */}
-      {name && (
-        <h2 className={style.name}>
-          {name}
-        </h2>
-      )}
-      {/* image */}
-      {image && (
-        <img className={style.image}
-          src={image}>
-        </img>
-      )}
-      {/* text */}
-      {text && (
-        <p className={style.text}>
-          {text}
-        </p>
-      )}
-    </li>
-  );
+/**@type {Card} */
+export const Card = {
+  Photo,
+  Todo,
+  Post,
 };
--- src/entities/Card/types.ts
+++ src/entities/Card/types.ts
@@ -1,6 +1,22 @@
-export type CardDetails = {
+export type PhotoDetails = {
   id: number;
-  name?: string;
-  image?: string;
-  text?: string;
+  name: string;
+  image: string;
+};
+
+export type TodoDetails = {
+  id: number;
+  name: string;
+};
+
+export type PostDetail = {
+  id: number;
+  name: string;
+  text: string;
+};
+
+export type Card = {
+  Photo: (props: PhotoDetails) => JSX.Element | null;
+  Todo: (props: TodoDetails) => JSX.Element | null;
+  Post: (props: PostDetail) => JSX.Element | null;
 };
