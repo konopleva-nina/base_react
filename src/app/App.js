6eab1a95fd53c5d88aacee5b041971cf16c552b7
Nina Konopleva <orangemise@gmail.com>
2024-11-03 23:16:55 +0300

    Refactored App component

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,8 @@
 import style from './App.module.scss';
 import { useEffect } from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import { usePhotos, useTodos } from 'shared/hooks';
-import { Gallery, Tasks } from 'widgets';
+import { HomePage, PhotoPage, PhotosPage, PostPage, PostsPage, TodoPage, TodosPage } from 'pages';
 
 /**
  * @typedef {import('./types').AppProps} AppProps
@@ -24,10 +25,36 @@ export const App = (props) => {
   }, []);
 
   return (
-    <div className={style.app}>
-      <h1>{props.name}</h1>
-      <Gallery />
-      <Tasks />
-    </div>
+    <BrowserRouter >
+      <div className={style.app}>
+        <h1>{props.name}</h1>
+        <nav className={style.nav}>
+          <ul>
+            <li>
+              <Link to={'/'}>Home</Link>
+            </li>
+            <li>
+              <Link to={'/photos'}>Gallery</Link>
+            </li>
+            <li>
+              <Link to={'/todos'}>Tasks</Link>
+            </li>
+            <li>
+              <Link to={'/posts'}>Posts</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path={'/'} element={<HomePage />}/>
+          <Route path={'/photos'} element={<PhotosPage />} />
+          <Route path={'/todos'} element={<TodosPage />} />
+          <Route path={'/posts'} element={<PostsPage />} />
+          <Route path={'/photo/:photoId'} element={<PhotoPage />} />
+          <Route path={'/todo/:todoId'} element={<TodoPage />} />
+          <Route path={'/post/:postId'} element={<PostPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
+
