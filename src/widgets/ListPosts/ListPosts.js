cebb1fe210bfa380cea379b7f2b73057da29dee5
Nina Konopleva <orangemise@gmail.com>
2024-11-23 22:13:49 +0300

    Refactored ListPosts

--- src/widgets/ListPosts/ListPosts.js
+++ src/widgets/ListPosts/ListPosts.js
@@ -1,8 +1,12 @@
 import style from './ListPosts.module.scss';
-import { usePosts } from 'shared/hooks';
-import { PostCounter, Posts } from 'features';
-import { Preloader } from 'shared/ui/Preloader';
+import { AppstoreAddOutlined } from 'shared/icons';
+import { usePosts } from 'shared/stores';
+import { useState } from 'react';
 import { useEffect } from 'react';
+import { Button } from 'shared/ui/Button';
+import { Card } from 'entities/index';
+import { Preloader } from 'shared/ui/Preloader';
+import { Post } from 'features';
 
 /**
  * @function ListPosts
@@ -11,18 +15,45 @@ import { useEffect } from 'react';
 
 export const ListPosts = () => {
   const postsStore = usePosts();
+  const [isPostCreationOpen, setIsPostCreationOpen] = useState(false);
 
   useEffect(() => {
     if (!postsStore.postCount) return;
     postsStore.getPosts(postsStore.postCount);
   }, [postsStore.postCount]);
 
+  const handlePostCreationClose = () => {
+    setIsPostCreationOpen(false);
+    postsStore.getPosts(postsStore.postCount);
+  };
+
   return (
     <div className={style.listPosts}>
-      <PostCounter />
+      {/* Counter */}
+      <Post.Counter />
+      {/* Create post button */}
+      <Button type={'button'}
+        onClick={() => setIsPostCreationOpen(true)}
+        isDisabled={postsStore.isPostsLoading}
+      >
+        <AppstoreAddOutlined />
+      </Button>
+      {/* Posts */}
       {!postsStore.postCount && <h2>Press the Up button to add photos</h2>}
-      <Posts posts={postsStore.posts} />
+      <ul className={style.posts}>
+        {postsStore.posts.map((post) => (
+          <Card.Post key={post.id}
+            id={post.id}
+            name={post.title}
+            text={post.body}
+          />
+        ))}
+      </ul>
       <Preloader isActive={postsStore.isPostsLoading} />
+      {/* Modal */}
+      <Post.Creator isOpen={isPostCreationOpen}
+        onClose={handlePostCreationClose}
+      />
     </div>
   );
 };
